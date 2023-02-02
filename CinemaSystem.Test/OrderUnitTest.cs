955c810e7a94d3df408c54566ec0f17caa99865d
Christian Nijssen <cwm.nijssen@student.avans.nl>
2023-02-02 16:19:35 +0100

    added multiple test functionality

--- CinemaSystem.Test/OrderUnitTest.cs
+++ CinemaSystem.Test/OrderUnitTest.cs
@@ -5,24 +5,52 @@ namespace CinemaSystem.Test;
 [TestFixture]
 public class OrderUnitTests
 {
-    private Order order;
-    private Movie movie;
-    private MovieScreening screening;
-    private MovieTicket ticket;
-    [SetUp] 
-    public void Setup()
+    private static IEnumerable<TestCaseData> CalculatePriceTestCaseData()
     {
-        movie = new Movie("Johan en het DotnetFramework");
-        screening = new MovieScreening(movie, DateTime.Today, 12);
-        ticket = new MovieTicket(screening, true, 12, 23);
-        order = new Order(324, true);
-        order.AddSeatReservation(ticket);
+        //Arrange
+        var movie = new Movie("Johan en het DotnetFramework");
+
+        var weekdayScreening = new MovieScreening(movie, new DateTime(2023, 2, 2), 10); // Thursday
+        var regularWeekdayTicket = new MovieTicket(weekdayScreening, false, 1, 1);
+        var premiumWeekdayTicket = new MovieTicket(weekdayScreening, true, 1, 2);
+        var weekdayTickets = new[] { regularWeekdayTicket, premiumWeekdayTicket };
+
+        var weekendScreening = new MovieScreening(movie, new DateTime(2023, 2, 4), 14); // Saturday
+        var regularWeekendTicket = new MovieTicket(weekendScreening, false, 1, 1);
+        var premiumWeekendTicket = new MovieTicket(weekendScreening, true, 1, 2);
+        var weekendTickets = new[] { regularWeekendTicket, premiumWeekendTicket };
+
+        var groupTicket1 = new MovieTicket(weekendScreening, false, 1, 1);
+        var groupTicket2 = new MovieTicket(weekendScreening, true, 1, 2);
+        var groupTicket3 = new MovieTicket(weekendScreening, true, 1, 3);
+        var groupTicket4 = new MovieTicket(weekendScreening, false, 1, 4);
+        var groupTicket5 = new MovieTicket(weekendScreening, false, 1, 5);
+        var groupTicket6 = new MovieTicket(weekendScreening, false, 1, 6);
+        var groupTicket7 = new MovieTicket(weekendScreening, false, 1, 7);
+        var groupTickets = new[]
+            { groupTicket1, groupTicket2, groupTicket3, groupTicket4, groupTicket5, groupTicket6, groupTicket7 };
+
+
+        yield return new TestCaseData(weekdayTickets, false).Returns(10d).SetName("Weekday normal order");
+        yield return new TestCaseData(weekdayTickets, true).Returns(10d).SetName("Weekend student order");
+        yield return new TestCaseData(groupTickets, false).Returns(93.60d).SetName("Weekend normal group-order");
+        yield return new TestCaseData(weekendTickets, false).Returns(31d).SetName("Weekend normal order");
     }
 
-    [Test]
-    public void AssertIfCalculatePriceReturns2()
+    [TestCaseSource(nameof(CalculatePriceTestCaseData))]
+    public double CalculatePrice_Correct(ICollection<MovieTicket> tickets, bool student)
     {
+        //Arrange
+        var order = new Order(1, student);
+        foreach (var movieTicket in tickets)
+        {
+            order.AddSeatReservation(movieTicket);
+        }
+
+        //Act
         var result = order.CalculatePrice();
-        Assert.That(result, Is.EqualTo(2));
+
+        //Assert
+        return result;
     }
 }
