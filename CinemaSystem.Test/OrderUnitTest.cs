0d67de18fa73b046932d5dacd1b4c01f618f315d
Jelmar1999 <62281610+Jelmar1999@users.noreply.github.com>
2023-02-02 10:45:18 +0100

    UnitTest Project added to solution

---
+++ CinemaSystem.Test/OrderUnitTest.cs
@@ -0,0 +1,15 @@
+namespace CinemaSystem.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
