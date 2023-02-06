dcc0f16d36c8adc858d246967380957355252594
Christian Nijssen <cwm.nijssen@student.avans.nl>
2023-02-06 18:02:00 +0100

    Remade tests

--- CinemaSystem.Test/OrderUnitTest.cs
+++ CinemaSystem.Test/OrderUnitTest.cs
@@ -11,46 +11,47 @@ public class OrderUnitTests
         var movie = new Movie("Johan en het DotnetFramework");
 
         var weekdayScreening = new MovieScreening(movie, new DateTime(2023, 2, 2), 10); // Thursday
-        var regularWeekdayTicket = new MovieTicket(weekdayScreening, false, 1, 1);
-        var premiumWeekdayTicket = new MovieTicket(weekdayScreening, true, 1, 2);
-        var weekdayTickets = new[] { regularWeekdayTicket, premiumWeekdayTicket };
-
         var weekendScreening = new MovieScreening(movie, new DateTime(2023, 2, 4), 14); // Saturday
-        var regularWeekendTicket = new MovieTicket(weekendScreening, false, 1, 1);
-        var premiumWeekendTicket = new MovieTicket(weekendScreening, true, 1, 2);
-        var weekendTickets = new[] { regularWeekendTicket, premiumWeekendTicket };
 
-        var groupTicket1 = new MovieTicket(weekendScreening, false, 1, 1);
-        var groupTicket2 = new MovieTicket(weekendScreening, true, 1, 2);
-        var groupTicket3 = new MovieTicket(weekendScreening, true, 1, 3);
-        var groupTicket4 = new MovieTicket(weekendScreening, false, 1, 4);
-        var groupTicket5 = new MovieTicket(weekendScreening, false, 1, 5);
-        var groupTicket6 = new MovieTicket(weekendScreening, false, 1, 6);
-        var groupTicket7 = new MovieTicket(weekendScreening, false, 1, 7);
+        var ticket1 = new MovieTicket(weekdayScreening, false, 1, 1);
+        var ticket2 = new MovieTicket(weekdayScreening, false, 1, 2);
+        var ticket3 = new MovieTicket(weekdayScreening, false, 1, 3);
+
+        var premiumTicket1 = new MovieTicket(weekdayScreening, true, 2, 1);
+        var premiumTicket2 = new MovieTicket(weekdayScreening, true, 2, 2);
+
+        var weekendTicket1 = new MovieTicket(weekendScreening, false, 1, 1);
+        var weekendTicket2 = new MovieTicket(weekendScreening, false, 1, 2);
+        var weekendTicket3 = new MovieTicket(weekendScreening, false, 1, 3);
+        var weekendTicket4 = new MovieTicket(weekendScreening, false, 1, 4);
+        var weekendTicket5 = new MovieTicket(weekendScreening, false, 1, 5);
+
+        var premiumWeekendTicket1 = new MovieTicket(weekendScreening, true, 2, 1);
+        var premiumWeekendTicket2 = new MovieTicket(weekendScreening, true, 2, 2);
+
+        var noTickets = Array.Empty<MovieTicket>();
+        var singleTicket = new[] { ticket1 };
+        var weekdayTickets = new[] { ticket1, ticket2, ticket3 };
+        var weekendTickets = new[] { weekendTicket1, weekendTicket2 };
+        var premiumTickets = new[] { premiumTicket1, premiumTicket2 };
+        var premiumWeekendTickets = new[] { premiumWeekendTicket1, premiumWeekendTicket2 };
         var groupTickets = new[]
-            { groupTicket1, groupTicket2, groupTicket3, groupTicket4, groupTicket5, groupTicket6, groupTicket7 };
+        {
+            weekendTicket1, weekendTicket2, weekendTicket3, weekendTicket4, weekendTicket5,
+            premiumWeekendTicket1, premiumWeekendTicket2
+        };
 
-
-        yield return new TestCaseData(weekdayTickets, false).Returns(10d).SetName("Weekday normal order");
-        yield return new TestCaseData(weekdayTickets, true).Returns(10d).SetName("Weekend student order");
-        yield return new TestCaseData(groupTickets, false).Returns(93.60d).SetName("Weekend normal group-order");
-        yield return new TestCaseData(weekendTickets, false).Returns(31d).SetName("Weekend normal order");
+        yield return new TestCaseData(noTickets, false).Returns(-1d).SetName("1. No tickets");
+        yield return new TestCaseData(singleTicket, false).Returns(10d).SetName("2. Single ticket");
+        yield return new TestCaseData(weekdayTickets, false).Returns(20d).SetName("3. Weekday");
+        yield return new TestCaseData(weekendTickets, false).Returns(28d).SetName("4. Weekend");
+        yield return new TestCaseData(weekdayTickets, true).Returns(20d).SetName("5. Weekday student");
+        yield return new TestCaseData(weekendTickets, true).Returns(14d).SetName("6. Weekend student");
+        yield return new TestCaseData(premiumTickets, false).Returns(13d).SetName("7. Premium weekday");
+        yield return new TestCaseData(premiumWeekendTickets, false).Returns(34d).SetName("8. Premium weekend");
+        yield return new TestCaseData(groupTickets, false).Returns(93.60d).SetName("9. Weekend large group");
     }
 
-    [Test(ExpectedResult = -1)]
-    public double CalculatePrice_NoTicketReturnsMinusOne()
-    {
-        //Arrange
-        //Order by default has no initialized tickets list, so it will always be null if no tickets are added
-        var order = new Order(1, false);
-    
-        //Act
-        var result = order.CalculatePrice();
-    
-        //Assert
-        return result;
-    }
-    
     [TestCaseSource(nameof(CalculatePriceTestCaseData))]
     public double CalculatePrice_Correct(ICollection<MovieTicket> tickets, bool student)
     {
@@ -67,5 +68,4 @@ public class OrderUnitTests
         //Assert
         return result;
     }
-    
 }
