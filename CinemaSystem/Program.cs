93db69a2c5b4ca0e8597a99276dfe3c147048b1a
Jelmar1999 <62281610+Jelmar1999@users.noreply.github.com>
2023-02-09 12:26:11 +0100

    Added dev qodana scan

--- CinemaSystem/Program.cs
+++ CinemaSystem/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CinemaSystem.Domain;
-using CinemaSystem.Domain.Strategy;
 
 Console.WriteLine("Hello, World!");
 var movie = new Movie("Johan en het DotnetFramework");
@@ -10,5 +9,3 @@ var ticket = new MovieTicket(screening, true, 12, 23);
 var order = new Order(324, true);
 order.AddSeatReservation(ticket);
 Console.WriteLine("Price = " + order.CalculatePrice());
-order.setExportBehaviour(new PlaintextExport());
-order.Export();
